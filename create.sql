5510a673e6390d0b4c040e0f62cfb414d4fd74bd
Nathaniel Brooke <neb17@duke.edu>
2018-10-08 16:12:47 -0400

    Updated location structure

--- create.sql
+++ create.sql
@@ -1,34 +1,33 @@
-CREATE TABLE country (
+CREATE TABLE location (
 	uid INT UNSIGNED NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
 	population BIGINT UNSIGNED,
 	avg_income DECIMAL(12,2),
-	gdp BIGINT UNSIGNED);
+	gdp BIGINT UNSIGNED
+	);
+
+CREATE TABLE country (
+	country_id INT UNSIGNED NOT NULL PRIMARY KEY REFERENCES location(uid)
+	);
 
 CREATE TABLE state (
-	uid INT UNSIGNED NOT NULL PRIMARY KEY,
-	country_id INT UNSIGNED NOT NULL REFERENCES country(uid),
-	name VARCHAR(256) NOT NULL,
-	population BIGINT UNSIGNED,
-	avg_income DECIMAL(12,2),
-	gdp BIGINT UNSIGNED);
+	state_id INT UNSIGNED NOT NULL PRIMARY KEY REFERENCES location(uid),
+	country_id INT UNSIGNED NOT NULL REFERENCES country(country_id));
 
 CREATE TABLE county (
-	uid INT UNSIGNED NOT NULL PRIMARY KEY,
-	state_id INT UNSIGNED NOT NULL REFERENCES state(uid),
-	name VARCHAR(256) NOT NULL,
-	population BIGINT UNSIGNED,
-	avg_income DECIMAL(12,2),
-	gdp BIGINT UNSIGNED);
+	county_id INT UNSIGNED NOT NULL PRIMARY KEY REFERENCES location(uid),
+	state_id INT UNSIGNED NOT NULL REFERENCES state(state_id)
+	);
 
 CREATE TABLE condition_name (
 	condition_id INT UNSIGNED NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(condition_id, name));
+	PRIMARY KEY(condition_id, name)
+	);
 
 CREATE TABLE datapoint (
 	condition_id INT UNSIGNED NOT NULL REFERENCES condition_name(condition_id),
-	location_id INT UNSIGNED NOT NULL, -- check reference/foreign key somehow
+	location_id INT UNSIGNED NOT NULL REFERENCES location(uid),
 	prevalence BIGINT UNSIGNED,
 	incidence BIGINT UNSIGNED,
 	mortality BIGINT UNSIGNED,
@@ -37,4 +36,5 @@ CREATE TABLE datapoint (
 	max_age TINYINT UNSIGNED NOT NULL,
 	gender CHAR(6) NOT NULL CHECK(gender = 'male' OR gender = 'female' OR gender = 'all'),
 	race_ethnicity VARCHAR(128) NOT NULL,
-	PRIMARY KEY(condition_id, location_id, year, min_age, max_age, gender, race_ethnicity));
+	PRIMARY KEY(condition_id, location_id, year, min_age, max_age, gender, race_ethnicity)
+	);

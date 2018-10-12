7285af2e7878a40b84996dcd4f62049f3e24708e
Nathaniel Brooke <neb17@duke.edu>
2018-10-11 23:11:55 -0400

    Fixed load.py

--- load.py
+++ load.py
@@ -7,16 +7,16 @@ cur = conn.cursor
 book = xlrd.open_workbook(filename="Sample_316_Data.xlsx")
 sheet_names = book.sheet_names()
 
-for i in range(1):
+for i in range(len(sheet_names)):
     sheet = book.sheet_by_index(i)
     name = sheet_names[i]
     titles = sheet.row(0)
-    print(titles)
     for row_idx in range(1, sheet.nrows):  # Iterate through rows
         curr_row = sheet.row(row_idx)
-        read_line(name, curr_row)
-        sql_code = 'INSERT INTO ' + name + ' VALUES '+ str(curr_row).replace('[', '(').replace(']', ')') + ';'
-        cur.execute(sql_code)
+        write_row = [str(value)[str(value).index(':')+1:(str(value)+'.').index('.')] for value in curr_row]
+        sql_code = 'INSERT INTO ' + name + ' VALUES '+ str(write_row).replace('[', '(').replace(']', ')').replace("'", "").replace('"', "'").replace('\\', '') + ';'
+        print(sql_code)
+        #cur.execute(sql_code)
 
 conn.close()
 

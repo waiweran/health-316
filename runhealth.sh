ec5011a8be4c8fd39b3ace926fe51677d523cea7
Gaby <Gaby@Gabrielas-MacBook-Pro.local>
2018-10-13 16:35:20 -0400

    Named Database

--- runhealth.sh
+++ runhealth.sh
@@ -1,8 +1,10 @@
-dropdb health
-createdb health
+dbname = health
+
+dropdb $dbname
+createdb $dbname
 psql health -af scripts/create.sql
 python load.py >load.sql
-psql health -af load.sql
+psql $dbname -af load.sql
 rm load.sql
-psql health -af scripts/Queries.sql >test-sample.out
+psql $dbname -af scripts/Queries.sql >test-sample.out
 echo "hot damn"

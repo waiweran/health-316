89a5e14054c28ca1e6c300a17775bc4f36b68fb5
Gaby <Gaby@Gabrielas-MacBook-Pro.local>
2018-10-24 23:32:40 -0400

    updated create and load

--- flask-nathaniel/create.sql
+++ flask-nathaniel/create.sql
@@ -1,25 +1,58 @@
-CREATE TABLE Bar(name VARCHAR(20) NOT NULL PRIMARY KEY,
-                 address VARCHAR(20));
-CREATE TABLE Beer(name VARCHAR(20) NOT NULL PRIMARY KEY,
-                  brewer VARCHAR(20));
-CREATE TABLE Drinker(name VARCHAR(20) NOT NULL PRIMARY KEY,
-                     address VARCHAR(20));
-CREATE TABLE Frequents(drinker VARCHAR(20) NOT NULL REFERENCES Drinker(name),
-                       bar VARCHAR(20) NOT NULL REFERENCES Bar(name),
-                       times_a_week SMALLINT CHECK(times_a_week > 0),
-                       PRIMARY KEY(drinker, bar));
-CREATE TABLE Serves(bar VARCHAR(20) NOT NULL REFERENCES Bar(name),
-                    beer VARCHAR(20) NOT NULL REFERENCES Beer(name),
-                    price DECIMAL(5,2) CHECK(price > 0),
-                    PRIMARY KEY(bar, beer));
-CREATE TABLE Likes(drinker VARCHAR(20) NOT NULL REFERENCES Drinker(name),
-                   beer VARCHAR(20) NOT NULL REFERENCES Beer(name),
-                   PRIMARY KEY(drinker, beer));
-CREATE TABLE Location(
-	uid CHAR(36) NOT NULL PRIMARY KEY,
+CREATE TABLE location (
+	uid CHAR(32) NOT NULL PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
-	type CHAR(32) NOT NULL,
-	population CHAR(32),
-	avg_income CHAR(32),
-	gdp CHAR(32)
-);
+	type CHAR(7) NOT NULL CHECK(type = 'country' OR type = 'state' OR type = 'county'),
+	population BIGINT,
+	avg_income DECIMAL(12,2),
+	gdp BIGINT
+	);
+
+CREATE TABLE in_location (
+	uid CHAR(32) NOT NULL PRIMARY KEY REFERENCES location(uid),
+	enclosing_id CHAR(32) NOT NULL REFERENCES location(uid)
+	);
+
+CREATE TABLE condition (
+	uid CHAR(32) NOT NULL PRIMARY KEY
+	);
+
+CREATE TABLE condition_name (
+	name VARCHAR(256) NOT NULL PRIMARY KEY,
+	condition_id CHAR(32) NOT NULL REFERENCES condition(uid)
+	);
+
+CREATE TABLE datapoint (
+	condition_id CHAR(32) NOT NULL REFERENCES condition(uid),
+	location_id CHAR(32) NOT NULL REFERENCES location(uid),
+	prevalence BIGINT,
+	incidence BIGINT,
+	mortality BIGINT,
+	year SMALLINT NOT NULL,
+	min_age SMALLINT NOT NULL,
+	max_age SMALLINT NOT NULL,
+	gender CHAR(6) NOT NULL CHECK(gender = 'male' OR gender = 'female' OR gender = 'all'),
+	race_ethnicity VARCHAR(128) NOT NULL,
+	PRIMARY KEY(condition_id, location_id, year, min_age, max_age, gender, race_ethnicity)
+	);
+
+CREATE FUNCTION TF_in_restriction() RETURNS TRIGGER AS $$
+BEGIN
+	IF EXISTS (SELECT * FROM location AS l1, location AS l2
+		WHERE l1.uid = NEW.uid AND l2.uid = NEW.enclosing_id
+		AND ((l1.type = 'state' AND l2.type = 'state')
+			OR (l1.type = 'country' AND l2.type = 'country')
+			OR (l1.type = 'county' AND l2.type = 'county')
+		 	OR (l1.type = 'country' AND l2.type = 'state')
+		 	OR (l1.type = 'country' AND l2.type = 'county')
+		 	OR (l1.type = 'state' AND l2.type = 'county'))
+		) THEN
+		RAISE EXCEPTION 'Invalid location hierarchy';
+	END IF;
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER TG_in_restriction
+	BEFORE INSERT OR UPDATE ON in_location
+	FOR EACH ROW
+	EXECUTE PROCEDURE TF_in_restriction();

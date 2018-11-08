365876ea6177031fa023b4c977246b2f7b0be000
Harry Liu <hkl6@duke.edu>
2018-11-08 01:38:31 -0500

    Place space and sample map for location subpage

--- flask-nathaniel/app.py
+++ flask-nathaniel/app.py
@@ -2,25 +2,32 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def main_page():
-	return render_template('main.html')
+    return render_template('main.html')
+
 
 @app.route('/locations')
 def locations_page():
-	return render_template('locations.html')
+    return render_template('locations.html')
+
 
 @app.route('/PMIdata')
 def pmi_page():
-	return render_template('PMIdata.html')
+    return render_template('PMIdata.html')
 
-@app.route('/process_data', methods=['GET','POST'])
+
+@app.route('/process_data', methods=['GET', 'POST'])
 def process_data():
-	
-	if request.method == 'POST': #this block is only entered when the form is submitted
-		conditionName = request.form.get('conditionName')
-		GDPrange = request.form.get('GDPrange')
-		return '<h1>The condition name you entered is: **{}** Your selected GDP value is **{}**'.format(conditionName,GDPrange)
+
+    if request.method == 'POST':  # this block is only entered when the form is submitted
+        conditionName = request.form.get('conditionName', 'None')
+    GDPrange = request.form.get('GDPrange')
+    AIrange = request.form.get('AIrange')
+    Poprange = request.form.get('Poprange')
+    return '''<h1> The condition name you entered is: **{}**. Your selected GDP value is **{}**. Your selected AI value is **{}**. Your selected Pop value is **{}**'''.format(conditionName, GDPrange, AIrange, Poprange)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')

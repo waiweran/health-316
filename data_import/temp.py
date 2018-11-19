0ad9a04f69b2db5bb7e4f887b2cfc19908cff496
Nathaniel Brooke <neb17@duke.edu>
2018-11-19 00:27:12 -0500

    Cleaning cancer files

---
+++ data_import/temp.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+conn = psycopg2.connect("dbname=health")
+c = conn.cursor()
+with open("state_cancer_incidence.csv") as file_in:
+	with open("state_cancer_incidence_2.csv", "w") as file_out:
+		file_out.write(file_in.readline())
+		for line in file_in:
+			location = split(line)[0]
+			c.execute("SELECT * FROM location WHERE name = %s", (location,))
+			if not c.fetchone() == None:
+				file_out.write(line)

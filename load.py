c3a61ee2e63492bb1d466f4bbc50066fc9c6e9f0
Nathaniel Brooke <neb17@duke.edu>
2018-11-18 22:23:47 -0500

    Found bug in load code

--- load.py
+++ load.py
@@ -5,5 +5,5 @@ cols = csv.readColumns("data_import/state_common_mortality.csv", {"locations": 4
 csv.loadDataPoints(locations=cols["locations"], location_type='state', conditions=cols["conditions"], years=cols["years"], mortalities=cols["mortalities"])
 
 csv.loadConditions("data_import/state_heartdisease.csv", 6)
-cols = csv.readColumns("data_import/state_common_mortality.csv", {"locations": 3, "conditions": 6, "years": 1, "mortalities": 8, "genders": 14, "ethn": 16})
+cols = csv.readColumns("data_import/state_heartdisease.csv", {"locations": 3, "conditions": 6, "years": 1, "mortalities": 8, "genders": 14, "ethn": 16})
 csv.loadDataPoints(locations=cols["locations"], location_type='state', conditions=cols["conditions"], years=cols["years"], mortalities=cols["mortalities"], genders=cols["genders"], race_ethnicities=cols["ethn"])

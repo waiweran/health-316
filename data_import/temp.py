84d09fa9f75c8822611d5bc933f761e8392fde7c
Nathaniel Brooke <neb17@duke.edu>
2018-11-19 00:29:09 -0500

    Bug fix

--- data_import/temp.py
+++ data_import/temp.py
@@ -6,7 +6,7 @@ with open("state_cancer_incidence.csv") as file_in:
 	with open("state_cancer_incidence_2.csv", "w") as file_out:
 		file_out.write(file_in.readline())
 		for line in file_in:
-			location = split(line)[0]
+			location = line.split(',')[0]
 			c.execute("SELECT * FROM location WHERE name = %s", (location,))
 			if not c.fetchone() == None:
 				file_out.write(line)

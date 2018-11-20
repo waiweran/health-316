74f05903839a94f96bd61890bc0bcca5025e9b24
Nathaniel Brooke <neb17@duke.edu>
2018-11-20 00:42:08 -0500

    Finished plotly utility

--- utilities/map_plot.py
+++ utilities/map_plot.py
@@ -34,8 +34,38 @@ def make_states_plot(plot_locations, plot_data, tooltip_labels, colorscale_label
 	             )
 	    
 	fig = dict( data=data, layout=layout )
-	return plotly.iplot(fig)
+	return plotly.plot(fig)
 
+def make_countries_plot(plot_locations, plot_data, tooltip_labels, colorscale_label, plot_title):
+	scl = [[0.0, 'rgb(242,240,247)'],[0.2, 'rgb(218,218,235)'],[0.4, 'rgb(188,189,220)'],\
+            [0.6, 'rgb(158,154,200)'],[0.8, 'rgb(117,107,177)'],[1.0, 'rgb(84,39,143)']]
 
-link = make_states_plot(['NC', 'MI', 'DC'], [2, 3, 15], ["Hi", "Foo", "Bar"], "Derp", "Blah my plot is cool")
-print(link)
+	data = [ dict(
+	        type = 'choropleth',
+	        locations = plot_locations,
+	        z = plot_data,
+	        text = tooltip_labels,
+	        colorscale = scl,
+	        autocolorscale = False,
+	        marker = dict(
+	            line = dict (
+	                color = 'rgb(0,0,0)',
+	                width = 2
+	            ) ),
+	        colorbar = dict(
+	            title = colorscale_label),
+	      ) ]
+
+	layout = dict(
+	    title = plot_title,
+	    geo = dict(
+	        showframe = False,
+	        showcoastlines = False,
+	        projection = dict(
+	            type = 'Mercator'
+	        )
+	    )
+	)
+	    
+	fig = dict( data=data, layout=layout )
+	return plotly.plot(fig)

a47deb5e7595c859aa742bc384f521b773bfd8ae
Nathaniel Brooke <neb17@duke.edu>
2018-11-28 23:27:04 -0500

    Fixed bugs in SQL queries

--- flask-nathaniel/database.py
+++ flask-nathaniel/database.py
@@ -5,7 +5,7 @@ def getDataTypes(condition_name):
     conn = psycopg2.connect("dbname=health")
     c = conn.cursor()
     c.execute('''
-        SELECT datapoint.type
+        SELECT DISTINCT type, name
         FROM datapoint, condition_name
         WHERE datapoint.condition_id = condition_name.condition_id
         AND condition_name.name = %s
@@ -13,6 +13,8 @@ def getDataTypes(condition_name):
     results = c.fetchall()
     c.close()
     conn.close()
+    types, names = zip(*results)
+    return types
 
 def getMapData(condition_name, data_type):
     '''Gets a dataset in map plotting format given a condition name and a data type'''
